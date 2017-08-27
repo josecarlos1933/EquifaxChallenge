b626efb42320788879dab9779eab219c2abb228a
root <root@jose-zuniga.c.august-outlet-134523.internal>
2017-08-27 01:33:04 +0000

    fixing bugs in index.js in order to search correctly the tickets

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -3,19 +3,18 @@ $("#btn_single_search").click(function(){
     type:"POST",
     contentType: "application/json",
     dataType: "json",
-    url:"http://localhost:3000/ok",
+    url:"/ok",
     data:JSON.stringify({issuekey:$("#input_single_key").val()}),
     success:function(result){
       fields=result.fields
-      //console.log(result)
+      console.log(result)
     $('#single_search_results').html(`
       <h3><b>Key</b>: ${result.key}</h3>
       <h3><b>Summary</b>: ${fields.summary}</h3>
+      <h3><b>Asignee</b>: ${fields.assignee.name}</h3>
+      <h3><b>Created</b>: ${fields.creator.name}</h3>
       <h3><b>Description</b>: ${fields.description}</h3>
       `)
-    },
-    error:function(x,status,error){
-      console.log('localhost is not accessible')
     }
   });
 
